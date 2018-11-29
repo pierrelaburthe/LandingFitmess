1dcd554427798c01e12a418fb294ffe842aec9d1
Pierre Laburthe <laburthe35@gmail.com>
2018-11-29 12:26:24 +0100

    killing jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,18 @@
 //= require rails-ujs
 //= require_tree .
+
+function initUpdateNavbarOnScroll() {
+  const navbar = document.querySelector('.navbar-wagon');
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.add('navbar-wagon-white');
+      } else {
+        navbar.classList.remove('navbar-wagon-white');
+      }
+    });
+  }
+}
+
+initUpdateNavbarOnScroll();
+
